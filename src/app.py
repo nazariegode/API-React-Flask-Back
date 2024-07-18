6c5fd270b0244daf9cfa4c80a45c29f59808f6c3
David Nazariego Ulacio <dnazariego@gmail.com>
2024-07-18 21:34:13 +0000

    Update

--- src/app.py
+++ src/app.py
@@ -1,15 +1,14 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
+import datetime
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -20,9 +19,11 @@ if db_url is not None:
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
 MIGRATE = Migrate(app, db)
 db.init_app(app)
+jwt = JWTManager(app)
 CORS(app)
 setup_admin(app)
 
@@ -44,6 +45,76 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/api/sign-up', methods=['POST'])
+def sign_up():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    is_active = request.json.get('is_active')
+
+    if not email:
+        return jsonify({"error": "Email is requerided!"}), 400
+    if not password:
+        return jsonify({"error": "password is requerided!"}), 400
+    if not is_active:
+        return jsonify({"error": "Is active is requerided!"}), 400
+
+    found = User.query.filter_by(email=email).first()
+    if found: 
+        return jsonify({"error": "Email is already exist!"}), 400
+
+    user = User(email=email, password=generate_password_hash(password), is_active=is_active)
+    user.save()
+    
+    if user: 
+        expires_at = datetime.timedelta(days=1)
+        access_token = create_access_token(identity=user.id, expires_delta=expires_at )
+
+        return jsonify({
+            "status": "success",
+            "message": "User registered successfully",
+            "access_token": access_token,
+            "currentUser": user.serialize()
+        }), 200 
+
+@app.route('/api/sign-in', methods=['POST'])
+def sign_in():
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    if not email:
+        return jsonify({"error": "Email is requerided!"}), 400
+    if not password:
+        return jsonify({"error": "password is requerided!"}), 400
+    print(email, password)
+
+    user = User.query.filter_by(email=email, is_active=True).first()
+    if not user: 
+        return jsonify({"error": "Incorrect Credentials!"}), 401
+    
+    if not check_password_hash(user.password, password): 
+        return jsonify({"error": "Incorrect Credentials!"}), 401
+    print(user.email)
+
+    if user: 
+        expires_at = datetime.timedelta(days=1)
+        access_token = create_access_token(identity=user.id, expires_delta=expires_at)
+
+        return jsonify({
+            "status": "success",
+            "message": "User registered successfully",
+            "access_token": access_token,
+            "currentUser": user.serialize()
+        }), 200 
+   
+    return jsonify({
+        "error": "Error"
+    })
+
+@app.route('/api/sign-out', methods=['POST'])
+def sign_out():
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(180), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -17,3 +18,14 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
